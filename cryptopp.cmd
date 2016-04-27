929ebfe99caa9e8442610c8f0d1abf6e2c7bc705
zabulus <zabulus@apriorit.com>
2016-04-27 09:03:49 +0300

    Made function call in cmd file

--- cryptopp.cmd
+++ cryptopp.cmd
@@ -1,6 +1,22 @@
-SETLOCAL
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET SolutionDir=%CD%\
-msbuild cryptopp.proj /t:BuildAll /verbosity:m
+
+RD /s /q .\Win32
+RD /s /q .\x64
+
+CALL :Build 
+CALL :Build "CRYPTOPP_NO_UNALIGNED_DATA_ACCESS"
+CALL :Build "NO_OS_DEPENDENCE"
+CALL :Build "CRYPTOPP_USE_FIPS_202_SHA3"
+CALL :Build "CRYPTOPP_NO_UNALIGNED_DATA_ACCESS;NO_OS_DEPENDENCE"
+CALL :Build "CRYPTOPP_NO_UNALIGNED_DATA_ACCESS;CRYPTOPP_USE_FIPS_202_SHA3"
+CALL :Build "NO_OS_DEPENDENCE;CRYPTOPP_USE_FIPS_202_SHA3"
+CALL :Build "CRYPTOPP_NO_UNALIGNED_DATA_ACCESS;NO_OS_DEPENDENCE;CRYPTOPP_USE_FIPS_202_SHA3"
+
+:Build
+SETLOCAL
+
+msbuild cryptopp.proj /t:BuildAll /verbosity:m /p:"PreprocessorDefinitions=%1"
 if %errorlevel% equ 1 exit /b %errorlevel%
 
 FOR %%F IN (Win32\DLL_Output\Debug,Win32\DLL_Output\Release,x64\DLL_Output\Debug,x64\DLL_Output\Release) DO (
@@ -14,7 +30,7 @@ FOR %%F IN (Win32\DLL_Output\Debug,Win32\DLL_Output\Release,x64\DLL_Output\Debug
 	rem dlltest.exe
 	rem if %errorlevel% equ 1 exit /b %errorlevel%
 	popd
-	rem RD /s /q %%F
+	RD /s /q %%F
 )
 
 FOR %%F IN (Win32\Output\Debug,Win32\Output\Release,x64\Output\Debug,x64\Output\Release) DO (
@@ -26,9 +42,8 @@ FOR %%F IN (Win32\Output\Debug,Win32\Output\Release,x64\Output\Debug,x64\Output\
 	cryptest.exe tv all
 	if %errorlevel% equ 1 exit /b %errorlevel%
 	popd
-	rem RD /s /q %%F
+	RD /s /q %%F
 )
-
-
+ENDLOCAL
 
 ENDLOCAL
